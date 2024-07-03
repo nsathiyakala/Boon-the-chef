26c29a29b0f6de3f4ae37153ce38afea7d430483
Sathiya kala <nsathiyakala6@gmail.com>
2024-07-03 21:29:47 +0530

    contact

--- Client/src/pages/contact.js
+++ Client/src/pages/contact.js
@@ -41,19 +41,19 @@ export default function Contact() {
                         <div className="col-12 col-lg-6">
                             <form className="contact-form">
                                 <div className="form-group">
-                                    <label htmlFor="name">Name</label>
+                                    {/* <label htmlFor="name">Name</label> */}
                                     <input type="text" id="name" name="name" placeholder="Enter your name" required />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="phone">Phone</label>
+                                    {/* <label htmlFor="phone">Phone</label> */}
                                     <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" required />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="email">Email</label>
+                                    {/* <label htmlFor="email">Email</label> */}
                                     <input type="email" id="email" name="email" placeholder="Enter your email address" required />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="message">Message</label>
+                                    {/* <label htmlFor="message">Message</label> */}
                                     <textarea id="message" name="message" rows="5" placeholder="Enter your message" required></textarea>
                                 </div>
                                 <button type="submit" className="btn btn-primary">Send Message</button>

4a9a80ccd71ddbe44c8c97c117c7873da811410b
mareeswari <jmareeswari0@gmail.com>
2024-07-05 15:19:20 +0530

    cloud issue solve

--- Backend/config/connectDatabase.js
+++ Backend/config/connectDatabase.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const connectDatabase=()=>{
-    mongoose.connect(process.env.DB_URL).then ((con)=>{
+    mongoose.connect(process.env.DB_URL    ).then ((con)=>{
         console.log(`Mongodb connection to host ${con.connection.host}`)
+    }).catch((err)=>{
+        console.log(`Moogodb connection error ${err.message}`);
     })
 }
 

7814b17598a44df0891ddb3c24de0ef30edd9859
mareeswari <jmareeswari0@gmail.com>
2024-07-05 13:43:12 +0530

    shippinginfo design

--- Client/src/pages/ShipppingInfo.jsx
+++ Client/src/pages/ShipppingInfo.jsx
@@ -28,82 +28,98 @@ const ShippingInfo = ({ cartItems }) => {
   };
 
   return (
-    <div className="shipping-info">
-      <h2>Shipping Information</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Full Name:</label>
-          <input
-            type="text"
-            name="fullName"
-            value={shippingDetails.fullName}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Address:</label>
-          <input
-            type="text"
-            name="address"
-            value={shippingDetails.address}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>City:</label>
-          <input
-            type="text"
-            name="city"
-            value={shippingDetails.city}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>State:</label>
-          <input
-            type="text"
-            name="state"
-            value={shippingDetails.state}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Zip Code:</label>
-          <input
-            type="text"
-            name="zipCode"
-            value={shippingDetails.zipCode}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Country:</label>
-          <input
-            type="text"
-            name="country"
-            value={shippingDetails.country}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Phone Number:</label>
-          <input
-            type="text"
-            name="phoneNumber"
-            value={shippingDetails.phoneNumber}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+    <div className="container">
+      <div>
+        <h2 className='text-center'>Shipping Information</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="row shipping-info mt-4">
+            <div className="col-lg-10">
+              <div className="row">
+                <div className="col-lg-6">
+
+                  <label>Full Name:</label>
+                  <input
+                    type="text"
+                    name="fullName"
+                    value={shippingDetails.fullName}
+                    onChange={handleChange}
+                    required
+                  />
+
+                </div>
+
+                <div className="col-lg-6">
+                  <label>Phone Number:</label>
+                  <input
+                    type="text"
+                    name="phoneNumber"
+                    value={shippingDetails.phoneNumber}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-lg-12">
+
+                  <label>Address:</label>
+                  <input
+                    type="text"
+                    name="address"
+                    value={shippingDetails.address}
+                    onChange={handleChange}
+                    required
+                  />
+
+                </div>
+                <div className="col-lg-6">
+                  <label>City:</label>
+                  <input
+                    type="text"
+                    name="city"
+                    value={shippingDetails.city}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+
+                <div className="col-lg-6" >
+                  <label>State:</label>
+                  <input
+                    type="text"
+                    name="state"
+                    value={shippingDetails.state}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-lg-6">
+                  <label>Zip Code:</label>
+                  <input
+                    type="text"
+                    name="zipCode"
+                    value={shippingDetails.zipCode}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-lg-6">
+                  <label>Country:</label>
+                  <input
+                    type="text"
+                    name="country"
+                    value={shippingDetails.country}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div> <button type="submit"> Check Out</button></div>
+               
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
+
   );
 };
 

8f753053d28f571f85ecd94580a1e97793a47e9a
Sathiya kala <nsathiyakala6@gmail.com>
2024-08-02 12:23:25 +0530

    refund policy in product details

--- Client/src/pages/productDetails.jsx
+++ Client/src/pages/productDetails.jsx
@@ -72,6 +72,16 @@ export default function ProductDetails({ cartItems, setCartItems }) {
             </Link>
           </div>
         </div>
+        <div className="row" style={{ padding: "0 20px" }}>
+          <p style={{ fontSize: "15px" }}>We have a 3-day return policy, which means you have 3 days after receiving your item to request a return. &nbsp;
+            <Link to="/refundpolicy" style={{ textDecoration: "none" }}> Learn more
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
+              </svg>
+            </Link>
+          </p>
+
+        </div>
       </div>
     </Fragment>
   );

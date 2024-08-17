e99bf97a576a9cc2d4ca6a92d087cfae14b08737
Sathiya kala <nsathiyakala6@gmail.com>
2024-08-17 14:52:18 +0530

    logo

--- Client/src/components/combo.jsx
+++ Client/src/components/combo.jsx
@@ -6,7 +6,7 @@ export default function Combo() {
             <section className="container">
                 <div className="row  combo">
                     <div className="  col-sm-9">
-                        <h1 className="text-start">Created your combo</h1>
+                        <h1 className="text-start">Create your combo</h1>
                     </div>
                     <div className="col-sm-3 ">
                         <button> <Link to={'/comboproducts'} className="text-center  p-5 no-underline">Create</Link>
